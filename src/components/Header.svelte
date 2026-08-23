d6fed8b4ca097fa06eee75299bcd4b82ee5d4a16
c43721 <rlahman.game@gmail.com>
2026-08-23 16:56:19 -0500

    feat: header and footer

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,223 +1,231 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { page } from '$app/stores';
-	interface Link {
-		name: string;
-		path: string;
-	}
+  import { onMount } from "svelte";
 
-	let links: Link[] = [
-		{ name: 'Home', path: '/' },
-		{ name: 'About', path: '/about' },
-		{ name: 'Projects', path: '/projects' },
-		{ name: 'Github', path: 'https://github.com/c43721' }
-	];
+  export let currentPath: string;
 
-	let showMobileMenu = false;
+  interface Link {
+    name: string;
+    path: string;
+  }
 
-	const handleMobileIconClick = () => (showMobileMenu = !showMobileMenu);
+  let links: Link[] = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Projects", path: "/projects" },
+    { name: "Github", path: "https://github.com/c43721" },
+  ];
 
-	const handleNavbarClick = () => {
-		if (!showMobileMenu) return;
-		showMobileMenu = !showMobileMenu;
-	};
+  let showMobileMenu = false;
 
-	const mediaQueryHandler = (e: MediaQueryListEvent) => {
-		if (!e.matches) {
-			showMobileMenu = false;
-		}
-	};
+  const handleMobileIconClick = () => (showMobileMenu = !showMobileMenu);
 
-	onMount(() => {
-		const mediaListener = window.matchMedia('(max-width: 450)');
+  const handleNavbarClick = () => {
+    if (!showMobileMenu) return;
+    showMobileMenu = !showMobileMenu;
+  };
 
-		mediaListener.addEventListener('change', mediaQueryHandler);
-	});
+  const mediaQueryHandler = (e: MediaQueryListEvent) => {
+    if (!e.matches) {
+      showMobileMenu = false;
+    }
+  };
+
+  onMount(() => {
+    const mediaListener = window.matchMedia("(max-width: 450)");
+
+    mediaListener.addEventListener("change", mediaQueryHandler);
+  });
 </script>
 
 <nav>
-	<div class="flex__container">
-		<div on:click={handleMobileIconClick} class="icon" class:mobile={showMobileMenu}>
-			<div class="middle-line" />
-		</div>
-		<ul class="navbar-list" class:mobile={showMobileMenu}>
-			{#each links as item}
-				<li>
-					<a on:click={handleNavbarClick} href={item.path} class:active={$page.path === item.path}
-						>{item.name}</a
-					>
-				</li>
-			{/each}
-		</ul>
-	</div>
+  <div class="flex__container">
+    <div
+      on:click={handleMobileIconClick}
+      class="icon"
+      class:mobile={showMobileMenu}
+    >
+      <div class="middle-line" />
+    </div>
+    <ul class="navbar-list" class:mobile={showMobileMenu}>
+      {#each links as item}
+        <li>
+          <a
+            on:click={handleNavbarClick}
+            href={item.path}
+            class:active={currentPath === item.path}>{item.name}</a
+          >
+        </li>
+      {/each}
+    </ul>
+  </div>
 </nav>
 
 <style lang="scss">
-	@import '../styles/base';
+  @import "../styles/base";
 
-	nav {
-		background-color: $primary;
-		height: 50px;
-	}
+  nav {
+    background-color: $primary;
+    height: 50px;
+  }
 
-	@media screen and (min-width: 750px) {
-		.flex__container {
-			max-width: 50vw;
-		}
-	}
+  @media screen and (min-width: 750px) {
+    .flex__container {
+      max-width: 50vw;
+    }
+  }
 
-	.flex__container {
-		margin: 0 auto;
-		padding: 0 20px;
-		display: flex;
-		align-items: center;
-		height: 100%;
-	}
+  .flex__container {
+    margin: 0 auto;
+    padding: 0 20px;
+    display: flex;
+    align-items: center;
+    height: 100%;
+  }
 
-	.icon {
-		width: 25px;
-		height: 14px;
-		position: relative;
-		cursor: pointer;
-	}
+  .icon {
+    width: 25px;
+    height: 14px;
+    position: relative;
+    cursor: pointer;
+  }
 
-	.icon:after,
-	.icon:before,
-	.middle-line {
-		content: '';
-		position: absolute;
-		width: 100%;
-		height: 2px;
-		background-color: $textSecondary;
-		transition: all 0.4s;
-		transform-origin: center;
-	}
+  .icon:after,
+  .icon:before,
+  .middle-line {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    background-color: $textSecondary;
+    transition: all 0.4s;
+    transform-origin: center;
+  }
 
-	.icon:before,
-	.middle-line {
-		top: 0;
-	}
+  .icon:before,
+  .middle-line {
+    top: 0;
+  }
 
-	.icon:after,
-	.middle-line {
-		bottom: 0;
-	}
+  .icon:after,
+  .middle-line {
+    bottom: 0;
+  }
 
-	.icon:before {
-		width: 66%;
-	}
+  .icon:before {
+    width: 66%;
+  }
 
-	.icon:after {
-		width: 33%;
-	}
+  .icon:after {
+    width: 33%;
+  }
 
-	.middle-line {
-		margin: auto;
-	}
+  .middle-line {
+    margin: auto;
+  }
 
-	.icon:hover:before,
-	.icon:hover:after,
-	.icon.active:before,
-	.icon.active:after,
-	.icon.active .middle-line {
-		width: 100%;
-	}
+  .icon:hover:before,
+  .icon:hover:after,
+  .icon.active:before,
+  .icon.active:after,
+  .icon.active .middle-line {
+    width: 100%;
+  }
 
-	.icon.active:before,
-	.icon.active:after {
-		top: 50%;
-		transform: rotate(-45deg);
-	}
+  .icon.active:before,
+  .icon.active:after {
+    top: 50%;
+    transform: rotate(-45deg);
+  }
 
-	.icon.active .middle-line {
-		transform: rotate(45deg);
-	}
+  .icon.active .middle-line {
+    transform: rotate(45deg);
+  }
 
-	.navbar-list {
-		display: none;
-		width: 100%;
-		justify-content: space-around;
-		margin: 0;
-		padding: 0 40px;
+  .navbar-list {
+    display: none;
+    width: 100%;
+    justify-content: space-around;
+    margin: 0;
+    padding: 0 40px;
 
-		& li {
-			list-style-type: none;
-			position: relative;
+    & li {
+      list-style-type: none;
+      position: relative;
 
-			& a.active {
-				color: $textPrimary;
-			}
-		}
+      & a.active {
+        color: $textPrimary;
+      }
+    }
 
-		& li::before {
-			content: '';
-			position: absolute;
-			bottom: 0;
-			left: 0;
-			width: 100%;
-			height: 1px;
-		}
+    & li::before {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 1px;
+    }
 
-		& li:hover::before {
-			transition: 250ms;
-			background-color: $textSecondary;
-		}
-	}
+    & li:hover::before {
+      transition: 250ms;
+      background-color: $textSecondary;
+    }
+  }
 
-	.navbar-list.mobile {
-		background-color: rgba(0, 0, 0, 0.7);
-		position: fixed;
-		display: block;
-		height: calc(100% - 50px);
-		bottom: 0;
-		left: 0;
-		display: flex;
-		gap: 15px;
-		flex-direction: column;
-		justify-content: start;
+  .navbar-list.mobile {
+    background-color: rgba(0, 0, 0, 0.7);
+    position: fixed;
+    display: block;
+    height: calc(100% - 50px);
+    bottom: 0;
+    left: 0;
+    display: flex;
+    gap: 15px;
+    flex-direction: column;
+    justify-content: start;
 
-		& li a {
-			font-size: 1.25rem;
-			transition: 250ms;
-			color: rgba(255, 255, 255, 0.8);
+    & li a {
+      font-size: 1.25rem;
+      transition: 250ms;
+      color: rgba(255, 255, 255, 0.8);
 
-			&:hover {
-				color: #fff;
-			}
+      &:hover {
+        color: #fff;
+      }
 
-			&.--active {
-				color: $textPrimary;
-			}
-		}
-	}
+      &.--active {
+        color: $textPrimary;
+      }
+    }
+  }
 
-	.navbar-list a {
-		color: $textSecondary;
-		font-weight: 600;
-		text-decoration: none;
-		display: flex;
-		height: 50px;
-		align-items: center;
-		padding: 0 10px;
-		font-size: 13px;
+  .navbar-list a {
+    color: $textSecondary;
+    font-weight: 600;
+    text-decoration: none;
+    display: flex;
+    height: 50px;
+    align-items: center;
+    padding: 0 10px;
+    font-size: 13px;
 
-		&:hover {
-			color: $textPrimary;
-		}
-	}
+    &:hover {
+      color: $textPrimary;
+    }
+  }
 
-	@media only screen and (min-width: 767px) {
-		.icon {
-			display: none;
-		}
+  @media only screen and (min-width: 767px) {
+    .icon {
+      display: none;
+    }
 
-		.navbar-list {
-			display: flex;
-			padding: 0;
-		}
+    .navbar-list {
+      display: flex;
+      padding: 0;
+    }
 
-		.navbar-list a {
-			display: inline-flex;
-		}
-	}
+    .navbar-list a {
+      display: inline-flex;
+    }
+  }
 </style>
