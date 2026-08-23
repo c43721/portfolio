14cea73a359734b610fad1ae57e45e91f7d23cb6
c43721 <rlahman.game@gmail.com>
2026-08-23 17:38:55 -0500

    feat: cf pages

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 
 import svelte from "@astrojs/svelte";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  adapter: cloudflare(),
 });
